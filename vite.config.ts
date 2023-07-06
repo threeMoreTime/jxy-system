3fce3828a0f6f015db0d0b59a7f782077a83c710
zhangsiqiang <1368376439@qq.com>
2023-07-06 09:50:18 +0800

    配置文件路径

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 // vite配置文件
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+// 引入node内置的模块path：获取绝对路径
+import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve:{
+    alias:{
+      // 将src路径配置为@符
+      '@':path.resolve(__dirname,'src')
+    }
+  }
 })
