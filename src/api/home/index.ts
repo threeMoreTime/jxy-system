43ebffbca50be4ad9c75cfa241abb94a497ac763
scgy <1368376439@qq.com>
2023-07-20 22:14:32 +0800

    新增类型TS文件 api接口类型补全

--- src/api/home/index.ts
+++ src/api/home/index.ts
@@ -1,8 +1,8 @@
 import requset from '@/utils/request'
-import internal from 'stream'
+import type {HospTailResponseData} from './type'
 enum API{
     // 获取现在有的医院数据的接口路径
     HOSPITAL_URL='/hosp/hospital/'
 }
 
-export const reqHospital = (page:number, limit:number)=>requset.get(API.HOSPITAL_URL+`${page}/${limit}`)
+export const reqHospital = (page:number, limit:number)=>requset.get<any,HospTailResponseData>(API.HOSPITAL_URL+`${page}/${limit}`)
---
+++ src/api/home/type.ts
@@ -0,0 +1,92 @@
+// 返回的基本信息
+export interface ResponseData {
+  code: number; //状态码
+  message: string; //消息
+  ok: boolean;
+}
+
+//已有的医院信息数据类型的TS类型
+export interface HospTailInfo {
+  id: string; // 医院 ID
+  createTime: string; // 创建时间
+  updateTime: string; // 更新时间
+  isDeleted: number; // 是否删除
+  param: {
+    hostypeString: string;
+    fullAddress: string;
+  }; // 医院的参数
+  hoscode: string; // 医院编码
+  hosname: string; // 医院名称
+  hostype: string; // 医院类型
+  provinceCode: string; // 省份编码
+  cityCode: string; // 城市编码
+  districtCode: string; // 地区编码
+  address: string; // 地址
+  logoData: string; // Logo 数据
+  intro: string; // 医院介绍（任意类型）
+  route: string; // 医院路线
+  status: number; // 医院状态
+  bookingRule: {
+    cycle: number;
+    releaseTime: string;
+    stopTime: string;
+    quitDay: number;
+    quitTime: string;
+    rule: string[]; // 预约规则
+  };
+}
+
+//存储全部已有医院的数组类型
+export type Content = HospTailInfo[];
+
+//获取全部已有医院的数组类型
+export interface  HospTailResponseData extends ResponseData{
+    data:{
+        content: Content
+        pageable: {
+            sort: {
+                sorted: boolean
+                unsorted: boolean
+                empty: boolean
+              },
+              "pageNumber": number
+              "pageSize": number
+              "offset": number
+              "paged": boolean
+              "unpaged": boolean
+        }
+        totalPages: number
+        totalElements: number
+        last: boolean
+        first: boolean
+        sort: {
+            "sorted": boolean,
+            "unsorted": boolean,
+            "empty": boolean
+          },
+        numberOfElements: number
+        size: number
+        number: number
+        empty: boolean
+    } 
+  }
+
+ //医院的参数类型
+export interface Param {
+  hostypeString: string; // 医院类型描述
+  fullAddress: string; // 完整地址
+}
+
+/**
+ * 预约规则类型
+ */
+export interface BookingRule {
+  cycle: number; // 规则周期
+  releaseTime: string; // 发布时间
+  stopTime: string; // 停止时间
+  quitDay: number; // 退订天数
+  quitTime: string; // 退订时间
+  rule: string[]; // 规则列表
+}
+
+
