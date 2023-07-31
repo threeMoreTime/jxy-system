9e2fa6963f49edfce14dc50134e7676be04fced0
zhangsiqiang <1368376439@qq.com>
2023-07-31 17:58:59 +0800

    新增搜索医院名称获取医院列表接口

--- src/api/home/index.ts
+++ src/api/home/index.ts
@@ -1,20 +1,21 @@
 import request from '@/utils/request';
-import type { HospTailResponseData } from './type';
+import type { HospTailResponseData,HospitalInfo } from './type';
 
 enum API {
   // 获取现在有的医院数据的接口路径
   HOSPITAL_URL = '/hosp/hospital/',
   // 数据字典接口路径
   HOSPITALLEVElANDREGION_URL= '/cmn/dict/findByDictCode/',
+  // 根据医院名称获取医院列表
+  HOSPITALINFO_URL='/hosp/hospital/findByHosname/'
 }
 
 // 获取分页列表信息
 export const reqHospital = (page: number, limit: number,hostype:string,districtCode:string) => request.get<any, HospTailResponseData>(API.HOSPITAL_URL + `${page}/${limit}?hostype=${hostype}&districtCode=${districtCode}`);
 //获取数据字典数据
 export const reqExportData = (dictCode:string) =>request.get<any>(API.HOSPITALLEVElANDREGION_URL + dictCode);
-//获取医院的等级或者获取医院地区的数据
-// export const reqHospitalLevelAndRegion = (dictCode: string) => request.get<any, HospitalLevelAndRegionResponseData>(API.HOSPITALLEVElANDREGION_URL + dictCode);
-
+//根据医院名称获取医院列表
+export const reqSearchInfo = (hosname:string) =>request.get<HospitalInfo>(API.HOSPITALINFO_URL + hosname);
 // 获取排班数据
 export const findScheduleList = (hoscode: string, depcode: string, workDate: string) => request.get<any>(API.HOSPITAL_URL+`/auth/findScheduleList/${hoscode}/${depcode}/${workDate}`);
 
