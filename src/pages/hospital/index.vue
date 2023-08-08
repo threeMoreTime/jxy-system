1dc97b9e21866b31f3f8efa5ab99a090ffa63bcd
zhangsiqiang <1368376439@qq.com>
2023-08-08 17:20:58 +0800

    register子组件信息渲染完毕

--- src/pages/hospital/index.vue
+++ src/pages/hospital/index.vue
@@ -3,7 +3,9 @@
     <!-- 左侧导航区域 -->
     <div class="menu">
       <div class="top">
-        <el-icon><HomeFilled /></el-icon>
+        <el-icon>
+          <HomeFilled />
+        </el-icon>
         <span> / 医院信息</span>
       </div>
       <el-menu :default-active="$route.path" class="el-menu-vertical-demo">
@@ -15,19 +17,27 @@
           <span>预约挂号</span>
         </el-menu-item>
         <el-menu-item index="/hospital/detail" @click="changeActive('/hospital/detail')">
-          <el-icon><document /></el-icon>
+          <el-icon>
+            <document />
+          </el-icon>
           <span>医院详情</span>
         </el-menu-item>
         <el-menu-item index="/hospital/notice" @click="changeActive('/hospital/notice')">
-          <el-icon><setting /></el-icon>
+          <el-icon>
+            <setting />
+          </el-icon>
           <span>预约通知</span>
         </el-menu-item>
         <el-menu-item index="/hospital/close" @click="changeActive('/hospital/close')">
-          <el-icon><InfoFilled /></el-icon>
+          <el-icon>
+            <InfoFilled />
+          </el-icon>
           <span>停诊信息</span>
         </el-menu-item>
         <el-menu-item index="/hospital/search" @click="changeActive('/hospital/search')">
-          <el-icon><Search /></el-icon>
+          <el-icon>
+            <Search />
+          </el-icon>
           <span>查询/取消</span>
         </el-menu-item>
       </el-menu>
@@ -52,9 +62,9 @@ import {
 } from "@element-plus/icons-vue";
 import { ref, onMounted } from "vue";
 // 引入路由
-import { useRouter,useRoute } from "vue-router";
+import { useRouter, useRoute } from "vue-router";
 // 引入仓库
-import useDetailStore from '@/store/moudues/hospitalDetail';
+import useDetailStore from "@/store/moudues/hospitalDetail";
 
 // 获取路由
 let $router = useRouter();
@@ -76,24 +86,26 @@ const handleClose = (key: string, keyPath: string[]) => {
 };
 
 // 跳转子路由
-const changeActive =(path:string)=>{
-$router.push(path)
-}
+const changeActive = (path: string) => {
+  $router.push(path);
+};
 </script>
-
 
 <style scoped lang="scss">
 .hospital {
   display: flex;
+
   .menu {
     flex: 2;
     display: flex;
     flex-direction: column;
     align-items: center;
+
     .top {
       color: #7f7f7f;
     }
   }
+
   .content {
     flex: 8;
   }
--- src/pages/hospital/register/index.vue
+++ src/pages/hospital/register/index.vue
@@ -1,17 +1,179 @@
 <template>
-    <div>
-        我是register组件
+  <div class="common-header">
+    <div class="title-wrapper">
+      <div class="hospital-title">
+        <span> {{ detailInfo.HospitalDetailInfo.hospital?.hosname }}</span>
+      </div>
+      <div class="icon-wrapper">
+        <svg
+          t="1691465135821"
+          class="icon"
+          viewBox="0 0 1024 1024"
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+          p-id="6818"
+          width="32"
+          height="32"
+        >
+          <path
+            d="M192.07 447.3m63.63 0l0 0q63.63 0 63.63 63.63l0 192.14q0 63.63-63.63 63.63l0 0q-63.63 0-63.63-63.63l0-192.14q0-63.63 63.63-63.63Z"
+            fill="#999999"
+            p-id="6819"
+          ></path>
+          <path
+            d="M449.43 320.29m63.63 0l0 0q63.63 0 63.63 63.63l0 319.16q0 63.63-63.63 63.63l0 0q-63.63 0-63.63-63.63l0-319.16q0-63.63 63.63-63.63Z"
+            fill="#999999"
+            p-id="6820"
+          ></path>
+          <path
+            d="M703.34 193.2m63.63 0l0 0q63.63 0 63.63 63.63l0 446.24q0 63.63-63.63 63.63l0 0q-63.63 0-63.63-63.63l0-446.24q0-63.63 63.63-63.63Z"
+            fill="#999999"
+            p-id="6821"
+          ></path>
+        </svg>
+        <span>
+          {{
+            detailInfo.HospitalDetailInfo.hospital?.param.hostypeString
+          }}</span
+        >
+      </div>
     </div>
+    <div class="Short-introduction"></div>
+  </div>
+  <div class="info-wrapper">
+    <!-- 字符串拼接base64编码图片格式 -->
+    <img
+      :src="`data:image/png;base64,${detailInfo.HospitalDetailInfo.hospital?.logoData}`"
+    />
+    <div class="content-wrapper">
+      <div>挂号规则</div>
+      <div class="line">
+        <span class="space"
+          >预约周期：<span class="label">{{
+            detailInfo.HospitalDetailInfo.bookingRule?.cycle
+          }}</span></span
+        >
+        <span class="space"
+          >放号时间：<span class="label">{{
+            detailInfo.HospitalDetailInfo.bookingRule?.releaseTime
+          }}</span></span
+        >
+        <span class="space"
+          >停挂时间：<span class="label">{{
+            detailInfo.HospitalDetailInfo.bookingRule?.stopTime
+          }}</span></span
+        >
+      </div>
+      <div class="line">
+        <span class="space"
+          >退号时间:就诊前一工作日<span class="label">{{
+            detailInfo.HospitalDetailInfo.bookingRule?.quitTime
+          }}</span
+          >前取消</span
+        >
+      </div>
+      <div style="margin-top: 3%">医院预约规则</div>
+      <div
+        class="rule-wrapper"
+        v-for="(ruler, index) in detailInfo.HospitalDetailInfo.bookingRule
+          ?.rule"
+        :key="index"
+      >
+        <ol>
+          <li>
+            {{ ruler }}
+          </li>
+        </ol>
+        <!--  -->
+      </div>
+    </div>
+  </div>
 </template>
 
-<script lang="ts">
-import { ref,onMounted } from 'vue'
+<script lang="ts" setup>
+// 引入FileSaver库
 
-export default {
-    name:'register'
-}
+import { ref, onMounted } from "vue";
+import useDetailStore from "@/store/moudues/hospitalDetail";
+name: "register";
+// 使用仓库
+let detailInfo = useDetailStore();
+
+onMounted(() => {});
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.common-header {
+  display: flex;
+  justify-content: space-between;
+  .title-wrapper {
+    display: flex;
+    align-items: center;
+    border: 1px dashed red;
+    .hospital-title {
+      span {
+        color: #333;
+        font-size: 20px;
+        font-weight: 700;
+      }
+    }
+    .icon-wrapper {
+      display: flex;
+      align-items: center;
+      color: #999;
+      font-size: 14px;
+    }
+  }
+}
+.info-wrapper {
+  width: 100%;
+  display: flex;
+  gap: 4%;
+  margin-top: 30px;
+  padding-left: 0%;
+  padding-right: 0%;
+  flex-direction: increment;
+  // 医院logo图片
+  img {
+    height: 80px;
+    width: 80px;
+    border-radius: 40px;
+  }
+  //   医院logo图片右侧信息
+  .content-wrapper {
+    //挂号规则信息
+    .line {
+      display: flex;
+      justify-content: space-between;
+      margin-top: 2%;
+      color: #999;
+      .space {
+        .label {
+          color: #333;
+        }
+      }
+    }
+    .rule-wrapper {
+      font-size: 12px;
+      margin-top: 8p;
+      min-height: 20px;
+      color: #999;
+      padding: 0;
+      background: none;
+      line-height: 20px;
+      ol {
+        line-height: 24px;
+        li {
+          margin-top: 3%;
+          list-style-type: decimal;
+          font-family: Helvetica Neue, Helvetica, Arial, PingFang SC,
+            Hiragino Sans GB, Heiti SC, Microsoft YaHei, WenQuanYi Micro Hei,
+            sans-serif;
+          display: list-item;
+          text-align: -webkit-match-parent;
+        }
+      }
+    }
+  }
+}
 </style>
